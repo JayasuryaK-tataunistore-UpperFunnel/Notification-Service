cd4342364841421a01c64423eb4fa490e399f521
JayasuryaK-tataunistore-UpperFunnel <k.jayasurya321@gmail.com>
2024-12-31 22:50:38 +0530

    notification service commit

---
+++ src/main/java/com/surya/notification/service/NotificationService.java
@@ -0,0 +1,53 @@
+package com.surya.notification.service;
+
+
+import com.surya.orderservice.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+
+public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent){
+        log.info("Got message from order-placed topic {}" , orderPlacedEvent);
+        //sending email to customer
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("testing@mail.com");
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
+            messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
+            messageHelper.setText(String.format("""
+                            Hi %s %s
+
+                            Your order with order number %s is now placed successfully.
+
+                            Best Regards
+                            Surya
+                            """,
+                    orderPlacedEvent.getFirstName().toString(),
+                    orderPlacedEvent.getLastName().toString(),
+                    orderPlacedEvent.getOrderNumber()));
+        };
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order Notification email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new RuntimeException("Exception occurred when sending mail to the given mail", e);
+        }
+    }
+
+
+}
